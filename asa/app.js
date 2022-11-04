bae3be5c570307d1fc9bd450660503e9b5ed4be1
PantsSleeve <stillaliveinthemetaverse@gmail.com>
2022-11-04 15:11:38 +0100

    ✨ Initial ASA commit

---
+++ asa/app.js
@@ -0,0 +1,47 @@
+import { } from './components/ps-card.js';
+import assets from './data/asa.json' assert { type: 'json' };
+import imgs from './data/svg.json' assert { type: 'json' };
+
+const content = document.getElementById('content');
+const filterText = document.getElementById('filter');
+const counter = document.getElementById('counter');
+const totalCount = 6969;
+
+filterText.onkeyup = (e) => {
+  if (e.key !== 'Enter' || e.keyCode !== 13) { return; }
+  let value = event.srcElement.value;
+  let filteredShits = Object.entries(assets).filter(a => {
+    return Object.values(a[1]).some(p => {
+      return p.toLowerCase().includes(value.toLowerCase());
+    })
+  });
+  updateCards(filteredShits);
+};
+
+const maxCardsToShow = 100;
+const updateCards = (filteredShits) => {
+  counter.innerHTML = `${filteredShits ? filteredShits.length : totalCount}/${totalCount}`
+  let c = document.createElement('div');
+  let shits = filteredShits || Object.entries(assets);
+  shits = shits.splice(0, maxCardsToShow);
+
+  for (let shit of shits) {
+    let key = shit[0];
+    let value = shit[1];
+
+    let card = document.createElement('ps-card');
+    let title = `<h3 slot="title">${value.Name}</h3>`;
+    let img = `<img slot="img" src="${imgs[key]}" />`;
+    let body = ``;
+    for (let prop of Object.entries(value)) {
+      if (prop[0] === "Name") { continue };
+      body += `<div>${prop[0]}: ${prop[1]}</div>`
+    }
+
+    card.innerHTML = `${title}${img}<div slot="body">${body}</div>`;
+    c.append(card);
+  }
+  content.innerHTML = c.innerHTML;
+}
+
+updateCards();
---
+++ asa/components/ps-card.js
@@ -0,0 +1,78 @@
+class PsCard extends HTMLElement {
+
+  constructor() {
+    super()
+    this._data;
+    this.render = this.render.bind(this)
+    this.attachShadow({ mode: 'open' })
+    this.render()
+  }
+
+  connectedCallback() {
+  }
+  disconnectedCallback() {
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+        }
+        .card {
+          margin: var(--space-s);
+          padding: 0;
+          color: var(--dark);
+          background: #fff;
+          height: 520px;
+          width: 320px;
+          position: relative;
+        }
+        .body {
+          padding: var(--space-s);
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          background: rgba(255,255,255,0.75);
+          font-size: 14px;
+          height: 160px;
+        }
+        ::slotted(h3) {
+          margin: 0!important;
+          padding: var(--space-m)!important;
+          font-size: 16px;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          background: rgba(255,255,255,0.75);
+        }
+
+        ::slotted(img) {
+          box-shadow: 0px 0px 2px 0px rgba(0,0,0,0.2);
+          width: 100%;
+          height: 400px;
+          object-fit: cover;
+          border: 1px solid rgba(255,255,255,0.3);
+          box-sizing: border-box;
+        }
+        /* Small screens */
+        @media only screen and (max-width: 600px) {
+          .card {
+            margin: 0 0 var(--space-s) 0;
+            width: 100%;
+          }
+        }
+      </style>
+      <div class="card">
+        <slot name="title"></slot>
+        <slot name="img"></slot>
+        <div class="body">
+          <slot name="body"></slot>
+        </div>
+      </div>
+    `
+  }
+}
+export default customElements.define('ps-card', PsCard)
